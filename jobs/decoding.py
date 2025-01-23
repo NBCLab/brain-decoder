c6fbe7ff36a0ce277899485db10749722b8d4b86
JulioAPeraza <julio.peraza@outlook.com>
2025-01-23 15:17:30 -0800

    Expand decoding to test on single-subject image

--- jobs/decoding.py
+++ jobs/decoding.py
@@ -1,41 +1,52 @@
+import os
 import os.path as op
 
 import numpy as np
+from nilearn.datasets import fetch_mixed_gambles
 
-from braindec.dataset import _get_vocabulary
 from braindec.predict import image_to_labels
 
 
 def main(project_dir):
     project_dir = op.abspath(project_dir)
     data_dir = op.join(project_dir, "data")
+    nilearn_dir = op.join(data_dir, "nilearn")
     results_dir = op.join(project_dir, "results")
-    output_dir = op.join(results_dir, "predictions")
-    metamaps_dir = "/Users/julioaperaza/Documents/GitHub/large-scale-ibma/data/neurosynth/metamaps"
+    images_source = "mixed-gambles"
+    output_dir = op.join(results_dir, "predictions", images_source)
     content = "abstract"
     source = "cogatlas"
-    term = "working_memory"
+
+    os.makedirs(output_dir, exist_ok=True)
+
     model_id = "mistralai/Mistral-7B-v0.1"
     model_name = model_id.split("/")[-1]
 
-    metamap = op.join(metamaps_dir, f"{term}.nii.gz")
+    if images_source == "mixed-gambles":
+        data = fetch_mixed_gambles(n_subjects=16, data_dir=nilearn_dir)
+        images = data.zmaps
+    else:
+        raise ValueError(f"Images source {images_source} not supported.")
 
     model_path = op.join(results_dir, "neurostore", f"best_clip-model_{content}_{model_name}.pth")
-    vocabulary = _get_vocabulary(source=source, data_dir=data_dir)
-    vocabulary_emb = np.load(op.join(data_dir, f"vocabulary_embedding_{model_name}.npy"))
+    vocabulary_emb = np.load(op.join(data_dir, f"vocabulary-{source}_embedding_{model_name}.npy"))
 
-    predictions_df = image_to_labels(
-        metamap,
-        model_path,
-        vocabulary,
-        vocabulary_emb,
-        data_dir=data_dir,
-    )
+    with open(op.join(data_dir, f"vocabulary-{source}_{model_name}.txt"), "r") as f:
+        vocabulary = [line.strip() for line in f]
 
-    predictions_df.to_csv(
-        op.join(output_dir, f"{term}_predictions_{content}_{model_name}.csv"),
-        index=False,
-    )
+    for img_i, img in enumerate(images):
+        predictions_df = image_to_labels(
+            img,
+            model_path,
+            vocabulary,
+            vocabulary_emb,
+            data_dir=data_dir,
+        )
+
+        predictions_df.to_csv(
+            op.join(output_dir, f"{img_i:02d}_predictions_{content}_{model_name}.csv"),
+            index=False,
+        )
 
 
 if __name__ == "__main__":
