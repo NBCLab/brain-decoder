c8607558781b8f523e58ee266b13be266adf9c4f
JulioAPeraza <julio.peraza@outlook.com>
2025-01-22 18:17:53 -0800

    Create create_nimare_dset.py

---
+++ jobs/create_nimare_dset.py
@@ -0,0 +1,40 @@
+import argparse
+import os.path as op
+
+from braindec.dataset import _neurostore_to_nimare
+
+
+def _get_parser():
+    parser = argparse.ArgumentParser(description="Run gradient-decoding workflow")
+    parser.add_argument(
+        "--project_dir",
+        dest="project_dir",
+        required=True,
+        help="Path to project directory",
+    )
+    return parser
+
+
+def main(project_dir):
+    project_dir = op.abspath(project_dir)
+    data_dir = op.join(project_dir, "data")
+    neurostore_dir = op.join(data_dir, "neurostore")
+
+    dset_pubget = _neurostore_to_nimare(neurostore_dir, source="pubget")
+    dset_pubget.save(op.join(data_dir, "neurostore-pubget_dset.pkl"))
+
+    dset_abstract = _neurostore_to_nimare(neurostore_dir, content="abstract")
+    dset_abstract.save(op.join(data_dir, "neurostore-abstract_dset.pkl"))
+
+    dset_body = _neurostore_to_nimare(neurostore_dir)
+    dset_body.save(op.join(data_dir, "neurostore-body_dset.pkl"))
+
+
+def _main(argv=None):
+    option = _get_parser().parse_args(argv)
+    kwargs = vars(option)
+    main(**kwargs)
+
+
+if __name__ == "__main__":
+    _main()
