1c3e4d9c1486c67ca2262fe4204e9eebaa3056c4
JulioAPeraza <julio.peraza@outlook.com>
2025-02-23 20:47:01 -0800

    Update decoding_hcp_nv.py

--- jobs/decoding_hcp_nv.py
+++ jobs/decoding_hcp_nv.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import os.path as op
 from glob import glob
@@ -21,16 +22,25 @@ def main():
     data_dir = op.join(project_dir, "data")
     voc_dir = op.join(data_dir, "vocabulary")
     results_dir = op.join(project_dir, "results")
-    section = "body"  # abstract, body
-    voc_source = "cogatlas"  # cogatlas, neurosynth
-    sub_category = "combined"  # "names", "combined"
+    sections = ["body"]
+    # sections = ["abstract", "body"]
+    voc_source = "cogatlas"
+    sub_categories = ["combined"]
+    # sub_categories = ["names", "definitions", "combined"]
     categories = ["task"]  # ["task", "concept"]
+    model_ids = [
+        "BrainGPT/BrainGPT-7B-v0.2",
+        # "mistralai/Mistral-7B-v0.1",
+        # "BrainGPT/BrainGPT-7B-v0.1",
+        # "meta-llama/Llama-2-7b-chat-hf",
+    ]
     topk = 20  # top k predictions
     standardize = False
     logit_scale = 20  # None
     device = "mps"
-    model_id = "BrainGPT/BrainGPT-7B-v0.2"  # BrainGPT/BrainGPT-7B-v0.2, mistralai/Mistral-7B-v0.1
-    model_name = model_id.split("/")[-1]
+
+    output_dir = op.join(results_dir, "predictions_hcp_nv")
+    os.makedirs(output_dir, exist_ok=True)
 
     dset = Dataset.load(op.join(data_dir, "dset-pubmed_annotated_nimare.pkl"))
 
@@ -40,27 +50,16 @@ def main():
         concept_snapshot=op.join(data_dir, "cognitive_atlas", "concept_snapshot-02-19-25.json"),
     )
 
-    # --------------------------------------------------------------------
-    # Set path to AI Decoder model and vocabulary
-    model_path = op.join(
-        results_dir,
-        "pubmed",
-        f"model-clip_section-{section}_embedding-{model_name}_best.pth",
-    )
-
-    output_dir = op.join(results_dir, "predictions_hcp_nv")
-    os.makedirs(output_dir, exist_ok=True)
-
     image_dir = op.join(data_dir, "hcp", "neurovault")
     images = sorted(glob(op.join(image_dir, "*.nii.gz")))
-    # images = images[:1]
 
-    for img_i, img_fn in enumerate(images):
+    for _, img_fn in enumerate(images):
         image_name = op.basename(img_fn).split(".")[0]
+        task_name = image_name.split("_")[1]
         # Plot map for debugging
         plot_surf(
             img_fn,
-            op.join(output_dir, f"{img_i:02d}_{image_name}_map.png"),
+            op.join(output_dir, f"{task_name}_map.png"),
             vmax=30,
         )
 
@@ -68,11 +67,19 @@ def main():
         if not check_same_fov(img, reference_masker=dset.masker.mask_img):
             img = resample_to_img(img, dset.masker.mask_img)
 
-        for category in categories:
+        for section, category, sub_category, model_id in itertools.product(
+            sections, categories, sub_categories, model_ids
+        ):
+            model_name = model_id.split("/")[-1]
+            model_path = op.join(
+                results_dir,
+                "pubmed",
+                f"model-clip_section-{section}_embedding-{model_name}_best.pth",
+            )
             vocabulary_lb = (
                 f"vocabulary-{voc_source}_{category}-{sub_category}_embedding-{model_name}"
             )
-            vocabulary_fn = op.join(voc_dir, f"{vocabulary_lb}.txt")
+            vocabulary_fn = op.join(voc_dir, f"vocabulary-{voc_source}_{category}.txt")
             vocabulary_emb_fn = op.join(voc_dir, f"{vocabulary_lb}.npy")
             vocabulary_prior_fn = op.join(voc_dir, f"{vocabulary_lb}_section-{section}_prior.npy")
             vocabulary, vocabulary_emb, vocabulary_prior = _read_vocabulary(
@@ -81,15 +88,13 @@ def main():
                 vocabulary_prior_fn,
             )
 
-            task_out_fn = f"{img_i:02d}_{vocabulary_lb}_section-{section}_pred-task_brainclip.csv"
+            task_out_fn = f"{task_name}_{vocabulary_lb}_section-{section}_pred-task_brainclip.csv"
             concept_out_fn = (
-                f"{img_i:02d}_{vocabulary_lb}_section-{section}_pred-concept_brainclip.csv"
+                f"{task_name}_{vocabulary_lb}_section-{section}_pred-concept_brainclip.csv"
             )
             process_out_fn = (
-                f"{img_i:02d}_{vocabulary_lb}_section-{section}_pred-process_brainclip.csv"
+                f"{task_name}_{vocabulary_lb}_section-{section}_pred-process_brainclip.csv"
             )
-            ns_out_fn = f"{img_i:02d}_vocabulary-{voc_source}_{category}-names_embedding-{model_name}_section-{section}_neurosynth.csv"
-            gclda_out_fn = f"{img_i:02d}_vocabulary-{voc_source}_{category}-names_embedding-{model_name}_section-{section}_gclda.csv"
 
             task_prob_df, concept_prob_df, process_prob_df = image_to_labels(
                 img,
@@ -110,9 +115,12 @@ def main():
             task_prob_df.to_csv(op.join(output_dir, task_out_fn), index=False)
             concept_prob_df.to_csv(op.join(output_dir, concept_out_fn), index=False)
             process_prob_df.to_csv(op.join(output_dir, process_out_fn), index=False)
-
+            """
             # Baseline
             # --------------------------------------------------------------------
+            ns_out_fn = f"{task_name}_vocabulary-{voc_source}_{category}-names_embedding-{model_name}_section-{section}_neurosynth.csv"
+            gclda_out_fn = f"{task_name}_vocabulary-{voc_source}_{category}-names_embedding-{model_name}_section-{section}_gclda.csv"
+
             # Load baseline model
             ns_model_fn = op.join(
                 results_dir,
@@ -124,15 +132,14 @@ def main():
                 "baseline",
                 f"model-gclda_{voc_source}-{category}_embedding-{model_name}_section-{section}.pkl",
             )
-
-            """
+            
             gclda_model = GCLDAModel.load(gclda_model_fn)
             gclda_predictions_df, _ = gclda_decode_map(gclda_model, img)
             gclda_predictions_df = gclda_predictions_df.sort_values(
                 by="Weight", ascending=False
             ).head(topk)
             gclda_predictions_df.to_csv(op.join(output_dir, gclda_out_fn), index=True)
-            """
+            
 
             ns_decoder = CorrelationDecoder.load(ns_model_fn)
             ns_predictions_df = ns_decoder.transform(img)
@@ -143,6 +150,7 @@ def main():
 
             ns_predictions_df = ns_predictions_df.sort_values(by="r", ascending=False).head(topk)
             ns_predictions_df.to_csv(op.join(output_dir, ns_out_fn), index=True)
+            """
 
 
 if __name__ == "__main__":
